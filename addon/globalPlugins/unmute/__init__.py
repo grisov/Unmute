1e745af567678a934e2bdbe990b6340985f8322b
grisov <grisov.gmail@mailnull.com>
2020-09-28 21:02:02 +0300

    Add-on initialization file

---
+++ addon/globalPlugins/unmute/__init__.py
@@ -0,0 +1,22 @@
+﻿#-*- coding:utf-8 -*-
+# A part of NonVisual Desktop Access (NVDA)
+# This file is covered by the GNU General Public License.
+# See the file COPYING for more details.
+# Copyright (C) 2020 Olexandr Gryshchenko <grisov.nvaccess@mailnull.com>
+
+import addonHandler
+from logHandler import log
+try:
+    addonHandler.initTranslation()
+except addonHandler.AddonError:
+    log.warning("Unable to initialise translations. This may be because the addon is running from NVDA scratchpad.")
+
+import os
+_addonDir = os.path.join(os.path.dirname(__file__), "..", "..")
+if isinstance(_addonDir, bytes):
+    _addonDir = _addonDir.decode("mbcs")
+_curAddon = addonHandler.Addon(_addonDir)
+_addonName = _curAddon.manifest['name']
+_addonSummary = _curAddon.manifest['summary']
+
+import globalPluginHandler
